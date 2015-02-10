8d544d2026e4371de156c095ec0be51e82143a9a
Hackbright Student <info@hackbrightacademy.com>
2015-02-09 16:39:34 -0800

    first table loading

--- seed.py
+++ seed.py
@@ -11,11 +11,21 @@ def load_movies(session):
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as rating_file:
+        reader = csv.reader(rating_file, delimiter='\t')
+        for row in reader:
+            rating = model.Rating(movie_id=row[1], user_id=row[0], rating=row[2])
+            session.add(rating)
+        session.commit()
+
+
+
+
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_ratings(session)
 
 if __name__ == "__main__":
     s= model.connect()
